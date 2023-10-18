f3b5a6dedc824f5cee968e90ba465ced67265ca2
leesin1040 <leesin1040@gmail.com>
2023-10-18 21:06:09 +0900

    자바스크립트 드가자~

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const options = {
     Authorization:
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjIyNTE0M2EzNTg3OGQxY2FkYTdmNjk4YmYwZWZhMCIsInN1YiI6IjY1MmY5OThjYTgwMjM2MDBjMzE2NWQ0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vMiFt7pND4DpoUQnFLKQbI03hux5EyIse35JoMwpfTA',
   },
-};
-
+// 드가자~;
 fetch(
   'https://api.themoviedb.org/3/movie/movie_id/lists?language=en-kr&page=1',
   options
